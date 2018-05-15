2f3452689118969938170db0139240758acfeb12
Bartlomiej Bialek <bialcus@gmail.com>
2018-05-15 11:50:29 +0100

    Add print weather on the screen.

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ import bs4
 
 def main():
     print_header()
-    location = input("What's your location? ")
-    html = get_html_from_web(location)
+    loc = input("What's your location? ")
+    html = get_html_from_web(loc)
+    get_weather_from_html(html)
 
 
 def print_header():
@@ -14,23 +15,32 @@ def print_header():
     print('--------------------')
 
 
-def get_html_from_web(location):
-    url = 'https://www.wunderground.com/weather/gb/{}'.format(location)
+def get_html_from_web(loc):
+    url = 'https://www.wunderground.com/weather/gb/{}'.format(loc)
     response = requests.get(url)
-    return response
+    return response.text
 
 
 def get_weather_from_html(html):
-    # cityCss = '.region-content-header h1'
-    # weatherScaleCss = '.wu-unit-temperature.wu-label'
-    # weatherTempCss = '.wu-unit-temperature.wu-value'
-    # weatherConditionCss = '.condition-icon'
-
     soup = bs4.BeautifulSoup(html, 'html.parser')
-    loc = soup.find(class_='region-content-header').find('h1').get_text()
+    loc = soup.find(class_='city-header').find('h1').get_text()
     condition = soup.find(class_='condition-icon').get_text()
     temp = soup.find(class_='wu-unit-temperature').find(class_='wu-value').get_text()
     scale = soup.find(class_='wu-unit-temperature').find(class_='wu-label').get_text()
+
+    loc = cleanup_text(loc)
+    condition = cleanup_text(condition)
+    temp = cleanup_text(temp)
+    scale = cleanup_text(scale)
+    print(loc, condition, temp, scale)
+
+
+def cleanup_text(text: str):
+    if not text:
+        return text
+
+    text = text.strip()
+    return text  
 
 
 if __name__ == '__main__':
