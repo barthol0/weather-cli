ad3c2888018a90d1fe3a069cc29547672f4603da
Bartlomiej Bialek <bialcus@gmail.com>
2018-05-15 01:35:56 +0100

    Add get weather from html.

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import requests
+import bs4
 
 
 def main():
     print_header()
     location = input("What's your location? ")
-    get_html_from_web(location)
+    html = get_html_from_web(location)
 
 
 def print_header():
@@ -16,7 +17,20 @@ def print_header():
 def get_html_from_web(location):
     url = 'https://www.wunderground.com/weather/gb/{}'.format(location)
     response = requests.get(url)
-    print(response.status_code)
+    return response
+
+
+def get_weather_from_html(html):
+    # cityCss = '.region-content-header h1'
+    # weatherScaleCss = '.wu-unit-temperature.wu-label'
+    # weatherTempCss = '.wu-unit-temperature.wu-value'
+    # weatherConditionCss = '.condition-icon'
+
+    soup = bs4.BeautifulSoup(html, 'html.parser')
+    loc = soup.find(class_='region-content-header').find('h1').get_text()
+    condition = soup.find(class_='condition-icon').get_text()
+    temp = soup.find(class_='wu-unit-temperature').find(class_='wu-value').get_text()
+    scale = soup.find(class_='wu-unit-temperature').find(class_='wu-label').get_text()
 
 
 if __name__ == '__main__':
